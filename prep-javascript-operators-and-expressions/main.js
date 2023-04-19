0d519b81589015d0b93ade171946fe57b843ef66
Kevin Unkle <kevinunk@gmail.com>
2023-04-19 21:10:21 +0000

    Completed prep-javascript-operators-and-variables exercise.

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,40 @@
+const number1 = 1;
+const number2 = 2;
+const product = number1 * number2;
+
+console.log(product);
+console.log(typeof product);
+
+const charge = 4;
+const payment = 2;
+const amountRemaining = charge - payment;
+console.log(amountRemaining);
+console.log(typeof amountRemaining);
+
+const tests = 5;
+const assignments = 6;
+const final = 10;
+const grade = (tests + assignments + final) / 3;
+console.log(grade);
+console.log(typeof grade);
+
+const firstName = 'Kevin';
+const lastName = 'Unkle';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+
+const pH = 5;
+const isAcidic = pH < 7;
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+const numberOfSoldiers = 100;
+const isSparta = numberOfSoldiers === 300;
+console.log(isSparta);
+console.log(typeof isSparta);
+
+const nickname = fullName;
+const isTheGoat = 'is the GOAT';
+console.log(nickname + ' ' + isTheGoat);
+console.log(typeof nickname);
